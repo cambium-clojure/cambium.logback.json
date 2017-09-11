252b60b4d247bc5fc35abde274c5a3ff72ed01c5
Shantanu Kumar <kumar.shantanu@gmail.com>
2017-09-11 16:01:55 +0530

    bump dependencies to respective GA versions

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/cambium-clojure/cambium.logback.json"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cambium/cambium.logback.core "0.4.0-SNAPSHOT"]
+  :dependencies [[cambium/cambium.logback.core "0.4.0"]
                  [com.fasterxml.jackson.core/jackson-core     "2.8.6"]  ; in use by cheshire 5.7.0
                  [com.fasterxml.jackson.core/jackson-databind "2.8.6"]  ; in use by cheshire 5.7.0
                  [ch.qos.logback.contrib/logback-json-classic "0.1.5"]
@@ -14,8 +14,8 @@
                 *assert* true
                 *unchecked-math* :warn-on-boxed}
   :profiles {:provided {:dependencies [[org.clojure/clojure  "1.5.1"]]}
-             :dev {:dependencies [[cambium/cambium.core "0.9.1-SNAPSHOT"]  ; pulls in [org.slf4j/slf4j-api "1.7.25"]
-                                  [cambium/cambium.codec-simple "0.9.1-SNAPSHOT"]]
+             :dev {:dependencies [[cambium/cambium.core "0.9.1"]  ; pulls in [org.slf4j/slf4j-api "1.7.25"]
+                                  [cambium/cambium.codec-simple "0.9.1"]]
                    :jvm-opts ["-Denable.dummy=true"]}
              :c15 {:dependencies [[org.clojure/clojure  "1.5.1"]]}
              :c16 {:dependencies [[org.clojure/clojure  "1.6.0"]]}
