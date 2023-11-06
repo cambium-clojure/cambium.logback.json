e3aa78e288ed9f92447a1df45fc2733b7927524f
Shantanu Kumar <kumar.shantanu@gmail.com>
2023-11-06 12:30:58 +0530

    update cambium.logback.core and Jackson depency versions

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/cambium-clojure/cambium.logback.json"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cambium/cambium.logback.core "0.4.5"]
-                 [com.fasterxml.jackson.core/jackson-core     "2.12.4"]  ; in use by cheshire 5.10.1
-                 [com.fasterxml.jackson.core/jackson-databind "2.12.4"]  ; in use by cheshire 5.10.1
+  :dependencies [[cambium/cambium.logback.core "0.4.6"]
+                 [com.fasterxml.jackson.core/jackson-core     "2.15.2"]  ; in use by cheshire 5.12.0
+                 [com.fasterxml.jackson.core/jackson-databind "2.15.2"]  ; in use by cheshire 5.12.0
                  [ch.qos.logback.contrib/logback-json-classic "0.1.5"]
                  [ch.qos.logback.contrib/logback-jackson      "0.1.5"]]
   :java-source-paths ["java-src"]
@@ -22,5 +22,6 @@
              :c08 {:dependencies [[org.clojure/clojure  "1.8.0"]]}
              :c09 {:dependencies [[org.clojure/clojure  "1.9.0"]]}
              :c10 {:dependencies [[org.clojure/clojure  "1.10.1"]]}
+             :c11 {:dependencies [[org.clojure/clojure  "1.11.1"]]}
              :dln {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
-  :aliases {"test-all" ["with-profile" "c06,dev:c07,dev:c08,dev:c09,dev:c10,dev" "test"]})
+  :aliases {"test-all" ["with-profile" "c06,dev:c07,dev:c08,dev:c09,dev:c10,dev:c11,dev" "test"]})
